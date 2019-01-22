d72e64237f9526654f007e19a2f3d422d956b8e1
lizhenghua-iri <lizhenghua-iri@360.cn>
2019-01-22 18:22:56 +0800

    update

--- app/src/javascripts/draw.js
+++ app/src/javascripts/draw.js
@@ -1,22 +1,70 @@
+// CanvasRenderingContext2D.prototype.wrapText = function (text, x, y, maxWidth, lineHeight) {
+//     if (typeof text != 'string' || typeof x != 'number' || typeof y != 'number') {
+//         return;
+//     }
+
+//     var context = this;
+//     var canvas = context.canvas;
+
+//     if (typeof maxWidth == 'undefined') {
+//         maxWidth = (canvas && canvas.width) || 300;
+//     }
+//     if (typeof lineHeight == 'undefined') {
+//         lineHeight = (canvas && parseInt(window.getComputedStyle(canvas).lineHeight)) || parseInt(window.getComputedStyle(document.body).lineHeight);
+//     }
+
+//     // 字符分隔为数组
+//     var arrText = text.split('');
+//     var line = '';
+
+//     for (var n = 0; n < arrText.length; n++) {
+//         var testLine = line + arrText[n];
+//         if (arrText[n+1])
+//         var metrics = context.measureText(testLine);
+//         var testWidth = metrics.width;
+//         if (testWidth > maxWidth && n > 0) {
+//             context.fillText(line, x, y);
+//             line = arrText[n];
+//             y += lineHeight;
+//         } else {
+//             line = testLine;
+//         }
+//     }
+//     context.fillText(line, x, y);
+// };
+
 module.exports = function (config) {
     var cvs = document.getElementById(config.cvs);
-    console.log(cvs)
     var ctx = cvs.getContext("2d");
-    var iw = config.iw;       // image's width
-    var ih = config.ih;       // image's height
-    var ew = config.ew;     // element's width
+    var iw = config.iw || 750;       // image's width
+    var ih = config.ih || 1334;       // image's height
+    var ew = config.ew || 390;     // element's width
     var eh = config.eh;     // element's height
-    var ex = config.ex;     // element's position x
-    var ey = config.ey;     // element's position y
+    var ex = config.ex || 185;     // element's position x
+    var ey = config.ey || 1040;     // element's position y
     var vw = document.body.clientWidth;     // viewport's width
     var vh = document.body.clientHeight;    // viewport's height
+    var lineh = 38;
+    var text1 = '美丫小姐姐在Adobe之神的庇护下';
+    var text2 = '工作更加得心应手，并获得';
+    var text3 = '每3稿出现1次1稿过的隐藏技能！';
     var img = new Image();
 
     img.src = "../images/1.jpg";
     // Content's width is bigger than height
     // if (vw/vh > iw/ih) {
     img.onload = function () {
-        ctx.drawImage(img, 0, 0);
+        ctx.drawImage(img, 0, 0, iw, ih);
+        ctx.font = '26px Arial';
+        ctx.fillStyle = '#fff';
+        ctx.textAlign = 'center';
+        ctx.shadowColor = '#1e49ac';
+        ctx.ShadowOffsetX = 1;
+        ctx.shadowOffsetY = 1;
+        ctx.shadowBlur = 6;
+        ctx.fillText(text1, iw / 2, ey, ew);
+        ctx.fillText(text2, iw / 2, ey + lineh, ew);
+        ctx.fillText(text3, iw / 2, ey + 2 * lineh, ew);
     }
     // }
 }
